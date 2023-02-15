f2310686a14529187d208dd1ed332ae68f76a7fe
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-02-14 20:29:47 -0500

    Proyecto 2

--- CLASE 2/app/src/main/java/com/example/myapplicationag2/MainActivity.java
+++ CLASE 2/app/src/main/java/com/example/myapplicationag2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplicationag2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "Actividaduno";
+    Button crearAlarma, llamar;
+    String phoneNumber = "3126373569";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "--Perrito--");
     }
 }
