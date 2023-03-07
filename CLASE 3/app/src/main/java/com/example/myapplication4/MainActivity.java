a2299815fd0b0143a5c6e18c6ff02cbd11545939
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-06 19:42:43 -0500

    Google y llamar
    Se ingresa nuevos botones

--- CLASE 3/app/src/main/java/com/example/myapplication4/MainActivity.java
+++ CLASE 3/app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -2,30 +2,39 @@ package com.example.myapplication4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.RemoteActionCompatParcelizer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+String TAG = "Depuracion";
 
-    String TAG = "Depuracion";
-    EditText Lado1, Lado2;
-    TextView Calcular;
-    Button Bottones;
+EditText Lado1, Lado2;
 
-    @Override
+TextView Calcular;
+
+Button Bottones;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         Log.i(TAG, "Estoy en OnCreate");
-        //Lado1 = findViewById(R.id.Resultadolinea1);
-        //Lado2= findViewById(R.id.Resultadolinea2);
-        //Calcular = findViewById(R.id.Calcular);
-        //Bottones = findViewById(R.id.Bottones);
+        Lado1 = findViewById(R.id.LadoOne);
+        Lado2 = findViewById(R.id.LAdotwo);
+        Calcular = findViewById(R.id.Calculemos);
+        Bottones = findViewById(R.id.Darle);
+
+
     }
+
+
 
     @Override
     protected void onStart() {
