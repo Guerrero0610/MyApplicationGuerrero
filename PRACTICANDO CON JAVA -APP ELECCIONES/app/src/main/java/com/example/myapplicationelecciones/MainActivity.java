42e979fe80dbda99fd95733dfbf209c6137769b2
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-10 18:19:03 -0500

    SE ORGANIZA SEGUNDA PANTALLA
    Se lleva la cedula que se ingresa a la segunda pantalla, se ingresa imagenes de candidatos a votar

--- PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/MainActivity.java
+++ PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/MainActivity.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Linea3;
+    EditText Linea3, Linea2;
     Button Boton1;
     int num=17;
 
@@ -22,14 +24,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Linea2=(EditText)findViewById (R.id.Linea2);
         Boton1=(Button)findViewById(R.id.Ingresar_1);
         Linea3=findViewById(R.id.Linea3);
 
         Boton1.setOnClickListener(new View.OnClickListener() {
-            
+
             @Override
             public void onClick(View view) {
+
                 String nume = Linea3.getText().toString();
                 int comparar = Integer.parseInt(Linea3.getText().toString());
 
@@ -41,11 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
                 } else{
                     Intent i = new Intent(MainActivity.this, Pantalla2.class);
+                    i.putExtra("Linea22", Linea2.getText().toString());
                     startActivity(i);
                 }
             }
         });
 
     }
+
 
 }
--- PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
+++ PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
@@ -3,12 +3,26 @@ package com.example.myapplicationelecciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Pantalla2 extends AppCompatActivity {
-
+    EditText Linea22;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+
+        Linea22 = (EditText) findViewById(R.id.Linea2);
+
+        String Lineac= getIntent().getStringExtra("Linea22");
+        publicarDatos(Lineac);
+
     }
+    public void publicarDatos(String Lineac){
+
+        Linea22.setText(Lineac);
+        
+    }
+
 }
