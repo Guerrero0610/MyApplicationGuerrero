fda0fb3745eeacee09eb4d1cd9fac444c8d01134
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-11 10:33:25 -0500

    YA SE PUEDE VOTAR POR UN CANDIDATO Y DEVUELVE AL MENU PRINCIPAL
    Se crea metodos para al darle click a una de las imagenes, indique por quien voto y agradezca, luego devuelva al menu principal para ingresar otro voto

--- PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
+++ PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
@@ -2,9 +2,12 @@ package com.example.myapplicationelecciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Pantalla2 extends AppCompatActivity {
     EditText Linea22;
@@ -23,6 +26,26 @@ public class Pantalla2 extends AppCompatActivity {
 
         Linea22.setText(Lineac);
         
+    }
+    public void Candidato1(View v){
+        Toast.makeText(this,"Has votado por Daniela Navardo",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Pantalla2.this, MainActivity.class);
+        startActivity(i);
+    }
+    public void Candidato2(View v){
+        Toast.makeText(this,"Has votado por Laura Bozzo",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Pantalla2.this, MainActivity.class);
+        startActivity(i);
+    }
+    public void Candidato3(View v){
+        Toast.makeText(this,"Has votado por Nacho Casano",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Pantalla2.this, MainActivity.class);
+        startActivity(i);
+    }
+    public void Candidato4(View v){
+        Toast.makeText(this,"Has votado en Blanco",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Pantalla2.this, MainActivity.class);
+        startActivity(i);
     }
 
 }
