9258f156981466c2336e493cb26906edf2e5994a
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-11 11:04:30 -0500

    ESPERA PARA SACAR MENSAJE DE POR QUIEN VOTO Y REGRESA A MENU
    Queria establecer un metodo que diera unos segundos para pasar de una interfaz a otra mientras se ejecuta el mensaje de por quien voto

--- PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
+++ PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla2.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.os.Handler;
 public class Pantalla2 extends AppCompatActivity {
     EditText Linea22;
     @Override
@@ -27,25 +27,53 @@ public class Pantalla2 extends AppCompatActivity {
         Linea22.setText(Lineac);
         
     }
+
     public void Candidato1(View v){
+
         Toast.makeText(this,"Has votado por Daniela Navardo",Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(Pantalla2.this, MainActivity.class);
-        startActivity(i);
+        Handler handler = new Handler();
+        int delayMillis = 2000;
+        Runnable runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Pantalla2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };;handler.postDelayed(runnable, delayMillis);
     }
     public void Candidato2(View v){
         Toast.makeText(this,"Has votado por Laura Bozzo",Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(Pantalla2.this, MainActivity.class);
-        startActivity(i);
+        Handler handler = new Handler();
+        int delayMillis = 2000;
+        Runnable runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Pantalla2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };;handler.postDelayed(runnable, delayMillis);
     }
     public void Candidato3(View v){
         Toast.makeText(this,"Has votado por Nacho Casano",Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(Pantalla2.this, MainActivity.class);
-        startActivity(i);
+        Handler handler = new Handler();
+        int delayMillis = 2000;
+        Runnable runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Pantalla2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };;handler.postDelayed(runnable, delayMillis);
     }
     public void Candidato4(View v){
         Toast.makeText(this,"Has votado en Blanco",Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(Pantalla2.this, MainActivity.class);
-        startActivity(i);
+        Handler handler = new Handler();
+        int delayMillis = 2000;
+        Runnable runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Pantalla2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };;handler.postDelayed(runnable, delayMillis);
     }
+
+
 
 }
