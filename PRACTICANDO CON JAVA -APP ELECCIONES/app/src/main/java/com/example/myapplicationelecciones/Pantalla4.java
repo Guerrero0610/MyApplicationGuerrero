c5682fb48d1db1eaed985d91eef50735b98cb3d2
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-11 18:06:21 -0500

    SE CREA ENLACE DE INTERFAZ 4 A MENU PRINCIPAL
    Se puede cerrar sesion

--- PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla4.java
+++ PRACTICANDO CON JAVA -APP ELECCIONES/app/src/main/java/com/example/myapplicationelecciones/Pantalla4.java
@@ -2,13 +2,25 @@ package com.example.myapplicationelecciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pantalla4 extends AppCompatActivity {
-
+    Button SALIRALMENU;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla4);
+
+        SALIRALMENU=(Button) findViewById(R.id.salirS);
+        SALIRALMENU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Pantalla4.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
