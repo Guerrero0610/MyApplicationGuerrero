2e814c6a52bd20b0581c4f6b44600680ec6ccdfc
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-13 16:33:30 -0500

    PANTALLA 3 TERMINADA
    Se termina mensajes personalizados

--- ENTREGABLE 12-03-2023/app/src/main/java/com/example/myapplicationbab/Pantalla3.java
+++ ENTREGABLE 12-03-2023/app/src/main/java/com/example/myapplicationbab/Pantalla3.java
@@ -2,17 +2,21 @@ package com.example.myapplicationbab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Pantalla3 extends AppCompatActivity {
     ImageView primero, segundo, tercero, primero1, segundo1, tercero1, primero2, segundo2, tercero2;
     EditText ENDIAS;
     Button VERLO;
+    TextView InfoBebe;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class Pantalla3 extends AppCompatActivity {
         tercero2 = (ImageView)findViewById(R.id.tercero111);
         ENDIAS = (EditText) findViewById(R.id.Diasl);
         VERLO = (Button)findViewById(R.id.Muestrame);
+        InfoBebe = (TextView)findViewById(R.id.informame);
 
         String Recibe = getIntent().getStringExtra("ENDIAS");
 
@@ -45,42 +50,52 @@ public class Pantalla3 extends AppCompatActivity {
                 if ( comparar >= 0 && comparar<=31) {
                     primero.setBackgroundResource(R.mipmap.primero);
                     primero.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("El embarazo está en sus primeras semanas");
 
                 } else if (comparar >= 32 && comparar<=62){
                     segundo.setBackgroundResource(R.mipmap.segundo);
                     segundo.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Ya se ha ido notando ese hermoso vientre");
 
                 }else if (comparar >= 63 && comparar<=93){
                     tercero.setBackgroundResource(R.mipmap.tercero);
                     tercero.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Mira el tamaño de tu vientre y tu bebe, es increible!");
 
                 }else if (comparar >= 94 && comparar<=124){
                     primero1.setBackgroundResource(R.mipmap.primero1);
                     primero1.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Sigue recomendaciones para que tu bebe siga creciendo y desarrollandose");
 
                 }else if (comparar >= 125 && comparar<=155){
                     segundo1.setBackgroundResource(R.mipmap.segundo1);
                     segundo1.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Mira lo grande que esta, ya este es el segundo Trimestre");
 
                 }else if (comparar >= 156 && comparar<=186){
                     tercero1.setBackgroundResource(R.mipmap.tercero1);
                     tercero1.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("El embarazo está en su punto donde el bebe empieza a acomodarse");
 
                 }else if (comparar >= 187 && comparar<=217){
                     primero2.setBackgroundResource(R.mipmap.primero2);
                     primero2.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Se acerca el día, que emoción no te parece?");
 
                 }else if (comparar >= 218 && comparar<=248){
                     segundo2.setBackgroundResource(R.mipmap.segundo2);
                     segundo2.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Que grande esta ¿Que tal esas pataditas?");
 
                 }else if (comparar >= 249 && comparar<=280){
                     tercero2.setBackgroundResource(R.mipmap.tercero2);
                     tercero2.setVisibility(View.VISIBLE);
+                    InfoBebe.setText("Estos son sus ultimos días aquí y pasaran a tus brazos");
 
                 }else {
-
+                    Toast.makeText(Pantalla3.this, "Tu bebe ya ha nacido, Felicidades", Toast.LENGTH_SHORT).show();
                 }
+                InfoBebe.setVisibility(View.VISIBLE);
 
 
             }
