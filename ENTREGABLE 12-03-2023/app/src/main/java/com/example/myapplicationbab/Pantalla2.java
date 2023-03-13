93dc4fb4e7ee7628b01c7243d47abd5901efb55d
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-13 00:22:04 -0500

    CALCULA CUANTOS DIAS LE FALTA DE EMBARAZO
    En la condicion se crea formula que calcule cuantos dias le falta para que el bebe nazca

--- ENTREGABLE 12-03-2023/app/src/main/java/com/example/myapplicationbab/Pantalla2.java
+++ ENTREGABLE 12-03-2023/app/src/main/java/com/example/myapplicationbab/Pantalla2.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Pantalla2 extends AppCompatActivity {
-    EditText RecibeNombre, RecibeDias, RecibeGenero;
+    EditText RecibeNombre, RecibeDias, RecibeGenero, LineaTiempo, LineaPeso, LineaTamaño;
     Button CALCULAR;
+    int FALTANTES_DIAS = 280, Total;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,11 @@ public class Pantalla2 extends AppCompatActivity {
         RecibeNombre = (EditText) findViewById(R.id.reciben);
         RecibeDias = (EditText) findViewById(R.id.recibed);
         RecibeGenero = (EditText) findViewById(R.id.recibeg);
+        LineaTiempo = (EditText) findViewById(R.id.lineaTiem);
+        LineaPeso = (EditText) findViewById(R.id.lineaP);
+        LineaTamaño = (EditText) findViewById(R.id.lineaTam);
         CALCULAR =(Button)findViewById(R.id.calculaPro);
+
 
         String Daton= getIntent().getStringExtra("RecibeNombre");
         String Datod= getIntent().getStringExtra("RecibeDias");
@@ -33,8 +39,55 @@ public class Pantalla2 extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+                String nume = getIntent().getStringExtra("RecibeDias");
+                int comparar = Integer.parseInt(RecibeDias.getText().toString());
 
+                if ( comparar > 0 && comparar<31) {
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "Entramos a condicion", Toast.LENGTH_SHORT).show();
 
+                } else if (comparar > 31 && comparar<62){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 62 && comparar<93){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 93 && comparar<124){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 124 && comparar<155){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 155 && comparar<186){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 186 && comparar<217){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 217 && comparar<248){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }else if (comparar > 248 && comparar<280){
+                    Total = FALTANTES_DIAS - comparar;
+                    LineaTiempo.setText(String.valueOf(Total));
+                    Toast.makeText(Pantalla2.this, "No se entro a condicion", Toast.LENGTH_SHORT).show();
+
+                }
 
             }
         });
