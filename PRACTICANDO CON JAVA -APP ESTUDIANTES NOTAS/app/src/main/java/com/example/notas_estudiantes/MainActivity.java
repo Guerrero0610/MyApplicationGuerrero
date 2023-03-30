29234175b427b9b3382510d09861e4ed90f78117
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-03-30 16:52:09 -0500

    SE CREA PANTALLA DE INICIO
    Se crea un Login para que el profesor inicie sesión

--- PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/MainActivity.java
+++ PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.notas_estudiantes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button BOTON_LOGIN;
 
+    EditText USUARIO, CONTRASEÑA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BOTON_LOGIN = (Button)findViewById(R.id.Boton_Ingresar);
+        USUARIO = (EditText)findViewById(R.id.Usuario);
+        CONTRASEÑA = (EditText)findViewById(R.id.Contraseña);
+
+        BOTON_LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = USUARIO.getText().toString();
+                String contra = CONTRASEÑA.getText().toString();
+
+                if ( usuario.equals("Luz Daneira") && contra.equals("1234")) {
+                    Intent i = new Intent(MainActivity.this, Pantalla_2.class);
+                    startActivity(i);
+
+                } else if (usuario.equals("")) {
+                    Toast.makeText(MainActivity.this, "Debe ingresar el usuario", Toast.LENGTH_SHORT).show();
+
+                }  else if (contra.equals("")) {
+                    Toast.makeText(MainActivity.this, "Debe ingresar la contraseña", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Usuario y/o contraseña incorrectos :(", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
---
+++ PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/Pantalla_2.java
@@ -0,0 +1,14 @@
+package com.example.notas_estudiantes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
