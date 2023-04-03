5f3e630d9cc5395e81fb58649cbcc6963eaeb5d1
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-04-02 20:32:08 -0500

    SE CREA BASE DE DATOS
    Se configura Base de Datos

---
+++ PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/BD_Estudiantes.java
@@ -0,0 +1,24 @@
+package com.example.notas_estudiantes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BD_Estudiantes extends SQLiteOpenHelper {
+
+    public BD_Estudiantes(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDatos) {
+        BaseDatos.execSQL("create table estudiante(Nombre text, Nota1 int, Nota2 int, Nota3 int, Nota4 int)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
--- PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/Pantalla_2.java
+++ PRACTICANDO CON JAVA -APP ESTUDIANTES NOTAS/app/src/main/java/com/example/notas_estudiantes/Pantalla_2.java
@@ -2,20 +2,85 @@ package com.example.notas_estudiantes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Pantalla_2 extends AppCompatActivity {
+
+    EditText NOTA11, NOTA22, NOTA33, NOTA44, INGRESE_ESTUDIANTE;
     TextView TRAER_PROFE;
+    Button BOTON_GUARDAR, BOTON_MOSTRAR, BOTON_CONSULTAR, BOTON_CERRAR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
 
+        INGRESE_ESTUDIANTE = (EditText)findViewById(R.id.IngresaEstudiante);
+        NOTA11 = (EditText)findViewById(R.id.nota1);
+        NOTA22 = (EditText)findViewById(R.id.nota2);
+        NOTA33 = (EditText)findViewById(R.id.nota3);
+        NOTA44 = (EditText)findViewById(R.id.nota4);
         TRAER_PROFE = (TextView) findViewById(R.id.Trae_profe);
         String DatoP= getIntent().getStringExtra("TRAER_PROFE");
 
         RecibeProfe(DatoP);
+    }
+    public void Resgistrar(View view){
+
+        BD_Estudiantes Listar = new BD_Estudiantes(this,"BaseDatos", null, 1);
+        SQLiteDatabase BaseDatos = Listar.getWritableDatabase();
+        String Nombre = INGRESE_ESTUDIANTE.getText().toString();
+        String Nota1 = NOTA11.getText().toString();
+        String Nota2 = NOTA22.getText().toString();
+        String Nota3 = NOTA33.getText().toString();
+        String Nota4 = NOTA44.getText().toString();
+
+        if (!Nombre.isEmpty() && !Nota1.isEmpty() && !Nota2.isEmpty() && !Nota3.isEmpty() && !Nota4.isEmpty()){
+            ContentValues registro = new ContentValues();
+            registro.put("Nombre", Nombre);
+            registro.put("Nota1", Nota1);
+            registro.put("Nota2", Nota2);
+            registro.put("Nota3", Nota3);
+            registro.put("Nota4", Nota4);
+            BaseDatos.insert("estudiante", null, registro);
+            BaseDatos.close();
+            INGRESE_ESTUDIANTE.setText("");
+            NOTA11.setText("");
+            NOTA22.setText("");
+            NOTA33.setText("");
+            NOTA44.setText("");
+            Toast.makeText(this,"Resgistro Almacenado Exitosamente", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Ingrese Correctamente Todos los Datos", Toast.LENGTH_LONG).show();
+        }
+
+    }
+    public void Consultar(View view){
+        BD_Estudiantes Listar = new BD_Estudiantes(this,"BaseDatos", null,1);
+        SQLiteDatabase BasedeDatos = Listar.getWritableDatabase();
+        String NombreE = INGRESE_ESTUDIANTE.getText().toString();
+        if (!NombreE.isEmpty()){
+            Cursor fila = BasedeDatos.rawQuery("select Nota1, Nota2, Nota3, Nota4 from estudiante where Nombre="+NombreE, null);
+            if (fila.moveToFirst()){
+                NOTA11.setText(fila.getString(0));
+                NOTA22.setText(fila.getString(1));
+                NOTA33.setText(fila.getString(2));
+                NOTA44.setText(fila.getString(3));
+                BasedeDatos.close();
+            }
+            else{
+                Toast.makeText(this,"No Se Encontro el Usuario, Por Favor Vuelva a Intentarlo", Toast.LENGTH_LONG).show();
+            }
+        }
     }
     public void RecibeProfe(String DatoP){
 
